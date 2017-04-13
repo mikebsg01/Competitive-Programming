5b514662ee100140eeb0429298c6429b8bd7a471
Michael Serrato <programador-experto97@hotmail.com>
2017-04-12 21:45:03 -0500

    Other files added

---
+++ others/CalculateCombinations.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int calc(int n) {
+  int sum = n, i;
+
+  if (i == 1) {
+    return 1;
+  }
+  
+  for (i = n; i >= 1; --i) {
+    sum += calc(i - 1);
+  }
+  return sum;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  cout << calc(n) << endl;
+  return 0;
+}
