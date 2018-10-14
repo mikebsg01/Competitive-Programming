03b6a98dfdaf5c75655e5d6d3bc5dd44b6a0ac49
Michael Serrato <mikebsg01@gmail.com>
2018-10-14 17:35:19 -0500

    omegaUp - TeamSkynet-Training-01 - Árbol Binario de Turing - Subtask 5 of 5 - AC

--- omegaUp/TeamSkynet-Training/Arbol-Binario-de-Turing.cpp
+++ omegaUp/TeamSkynet-Training/Arbol-Binario-de-Turing.cpp
@@ -1,275 +1,102 @@
 #include <bits/stdc++.h>
-#define dd(x) cout << #x << ": " << x << endl;
 #define optimize_ios ios_base::sync_with_stdio(0);cin.tie(0);
-#define MAXN 1000002
-#define MOD 21092013
+#define dd(x) cout << #x << ": " << x << endl;
 #define PB push_back
 #define MP make_pair
-#define NODE first.first
-#define PARENT first.second
-#define LEFT second.first
-#define RIGHT second.second
+#define X first
+#define Y second
 #define ALL(x) x.begin(), x.end()
-#define INF -1
 #define L 0
 #define R 1
+#define INF -1
+#define MOD 21092013
 using namespace std;
-typedef stack<bool> stackbl;
-typedef stack<int> stacki;
-typedef vector<bool> vectorbl;
+typedef stack<short> stacki;
+typedef pair<int, int> Pair;
+typedef pair<int, Pair> Tern;
 
 string S, T;
-stackbl ParentDirections;
-vectorbl ChildrenDirections;
-stacki MovsLater;
-bool direction;
-int DP[MAXN];
-int lastOccurrence[3];
-
-inline int getIdx(char c) {
-	return c == 'L' ? 0 : (c == 'R' ? 1 : 2);
-}
-
-inline char toChar(bool idx) {
-	return idx ? 'R' : 'L';
-}
-
-inline bool isMovDown(char c) {
-	return c == 'L' or c == 'R';
-}
-
-bool dontHaveMovUp() {
-	int i, n = T.size();
-	bool res = true;
-
-	for (i = 0; i < n; ++i) {
-		if (!isMovDown(T[i])) {
-			res = false;
-		} else {
-			direction = getIdx(T[i]);
-
-			if (! res) {
-				return res;
-				break;
-			}
-		}
-	}
-
-	return res;
-}
-
-inline int modMult(int a, int b) {
-	return ((a % MOD) * (b % MOD)) % MOD;
-}
+stacki ParentDirections;
+vector<int> DP;
+int last[3];
+int ans;
 
 inline int modPositive(int n) {
 	return ((n % MOD) + MOD) % MOD;
 }
 
 inline int modSub(int a, int b) {
-	return ((a % MOD) - (b % MOD)) % MOD;
+	return modPositive(((a % MOD) - (b % MOD)) % MOD);
 }
 
 inline int modSum(int a, int b) {
 	return ((a % MOD) + (b % MOD)) % MOD;
 }
 
-int countDistinctSubsequences() {
-	int i, n, charIdx;
+inline int modMult(int a, int b) {
+	return ((a % MOD) * (b % MOD)) % MOD;
+}
 
-	n = T.size(), DP[0] = 1;
+inline short getIdx(char c) {
+	return (c == 'L' ? 0 : ((c == 'R') ? 1 : 2));
+}
 
-	for (i = 1; i <= n; ++i) {
-		DP[i] = modMult(2, DP[i - 1]);
-		charIdx = getIdx(T[i - 1]); 
-
-		if (lastOccurrence[charIdx] > 0) {
-			DP[i] = modPositive(modSub(DP[i], DP[lastOccurrence[charIdx] - 1]));
-		}
-
-		lastOccurrence[charIdx] = i;
-	}
-
-	return DP[n];
+inline bool isUp(char c) {
+	return c == 'U';
 }
 
 void getFinalDirections() {
 	int i, n = S.size();
 
 	for (i = 0; i < n; ++i) {
-		if (isMovDown(S[i])) {
+		if (! isUp(S[i])) {
 			ParentDirections.push(getIdx(S[i]) ^ 1);
 		} else if (! ParentDirections.empty()) {
 			ParentDirections.pop();
 		}
 	}
-
-	stackbl tmp(ParentDirections);
-
-	while (!tmp.empty()) {
-		ChildrenDirections.PB(tmp.top() ^ 1);
-		tmp.pop();
-	}
-
-	reverse(ALL(ChildrenDirections));
 }
 
-void findMovsLater() {
-	int i, n = T.size(), movs = 0;
+void calculate() {
+	int n, i, j, idx, lastLIdx = INF, lastRIdx = INF, subsequencesCount, dpIdx;
+	vector<Tern> uIdx;
 
-	for (i = n - 1; i >= 0; --i) {
-		if (isMovDown(T[i])) {
-			++movs;
-		} else {
-			MovsLater.push(movs);
-		}
-	}
+	DP.PB(1);
 
-	MovsLater.push(movs);
-}
+	for (i = T.size() - 1, j = 1; i >= 0; --i) {
+		idx = getIdx(T[i]);
 
-int countDistinctNodes() {
-	int movs, res = 0, i = 0, parentDirection;
+		if (! isUp(T[i])) {
+			DP.PB(modMult(2, DP[j - 1]));
 
-	while ((! MovsLater.empty()) and (! ParentDirections.empty())) {
-		movs = MovsLater.top();
-		MovsLater.pop();
-
-		if (i == 0) {
-			res = modSum(res, modSum(movs, 1));
-		} else {
-			parentDirection = ParentDirections.top();
-			ParentDirections.pop();
-
-			if (parentDirection == direction) {
-				res = modSum(res, movs);
+			if (last[idx] > 0) {
+				DP[j] = modSub(DP[j], DP[last[idx] - 1]);
 			}
 
-			res = modSum(res, 1);
-		}
-
-		++i;
-	}
-
-	return res;
-}
-
-typedef pair<int, int> Pair;
-typedef pair<Pair, Pair> Tern;
-
-vector<Tern> BT;
-vector<string> Subsequences;
-set<int> ans;
-
-int createNode(int val) {
-	int idx = BT.size();
-	BT.PB(MP(MP(val, idx), MP(INF, INF)));
-	return idx;
-}
-
-int generateTree(int node, int level) {
-	if (level >= ChildrenDirections.size()) {
-		return node;
-	}
-
-	bool direction = ChildrenDirections[level];
-
-	if (direction == L) {
-		int leftIdx = createNode(level + 1);
-		BT[leftIdx].PARENT = node;
-		BT[node].LEFT = leftIdx;
-		return generateTree(leftIdx, level + 1);
-	}  else {
-		int rightIdx = createNode(level + 1);
-		BT[rightIdx].PARENT = node;
-		BT[node].RIGHT = rightIdx;
-		return generateTree(rightIdx, level + 1);
-	}
-}
-
-void printTree() {
-	int i, n = BT.size();
-
-	cout << "Tree: " << endl;
-	for (i = 0; i < n; ++i)
-		cout << setw(2) << BT[i].NODE << " ";
-	cout << endl;
-	for (i = 0; i < n; ++i)
-		cout << setw(2) << BT[i].PARENT << " ";
-	cout << endl;
-	for (i = 0; i < n; ++i)
-		cout << setw(2) << BT[i].LEFT << " ";
-	cout << endl;
-	for (i = 0; i < n; ++i)
-		cout << setw(2) << BT[i].RIGHT << " ";
-	cout << endl;
-}
-
-void generateAllSubsequences() {
-	int i, j, n = T.size(), iter = 0;
-	string s;
-	set<string> heap;
-
-	//cout << "All Subs: " << endl;
-	for (i = 0; i < (1 << n); ++i) {
-			s = "";
-
-			for (j = 0; j < n; ++j) {
-				if ((i & (1 << j)) > 0) {
-					s += T[j];
-				}
+			if (idx == L) {
+				lastLIdx = j;
+			} else {
+				lastRIdx = j;
 			}
 
-			heap.insert(s);
-			//cout << ">" << s << endl;
-	}
-	//cout << endl;
-
-	for (set<string>::iterator it = heap.begin(); it != heap.end(); ++it) {
-		Subsequences.PB((*it));
-	}
-}
-
-void travel(int node, int subsequenceIdx, int level) {
-	if (node != INF) {
-		ans.insert(node);
-	}
-
-	if (level >= Subsequences[subsequenceIdx].size()) {
-		return;
-	}
-
-	char c = Subsequences[subsequenceIdx][level];
-	//cout << c << " ";
-
-	if (c == 'U') {
-		travel(BT[node].PARENT, subsequenceIdx, level + 1);
-	} else if (c == 'L') {
-		if (BT[node].LEFT == INF) {
-			int leftIdx = createNode(BT.size());
-			BT[leftIdx].PARENT = node;
-			BT[node].LEFT = leftIdx;
+			last[idx] = j++;
+		} else {
+			uIdx.PB(MP(j - 1, MP(lastLIdx, lastRIdx)));
 		}
-
-		travel(BT[node].LEFT, subsequenceIdx, level + 1);
-	} else {
-		if (BT[node].RIGHT == INF) {
-			int rightIdx = createNode(BT.size());
-			BT[rightIdx].PARENT = node;
-			BT[node].RIGHT = rightIdx;
-		}
-
-		travel(BT[node].RIGHT, subsequenceIdx, level + 1);
 	}
-}
 
-void findDistinctNodes(int currentNode) {
-	int i, n = Subsequences.size();
+	ans = modSum(ans, DP[j - 1]);
 
-	for (i = 0; i < n; ++i) {
-		//cout << ">";
-		travel(currentNode, i, 0);
-		//cout << endl;
+	for (i = uIdx.size() - 1; i >= 0 and ! ParentDirections.empty(); --i) {
+		idx = ParentDirections.top(); ParentDirections.pop();
+		subsequencesCount = DP[uIdx[i].X];
+		dpIdx = idx == R ? uIdx[i].Y.X : uIdx[i].Y.Y;
+
+		if (dpIdx == INF) {
+			ans = modSum(ans, subsequencesCount);
+		} else {
+			ans = modSum(ans, modSub(subsequencesCount, DP[dpIdx - 1]));
+		}
 	}
 }
 
@@ -279,26 +106,8 @@ int main() {
 	cin >> S >> T;
 
 	getFinalDirections();
+	calculate();
 
-	if (T.size() <= 20) {
-		int root = createNode(0);
-		int currentNode = generateTree(root, 0);
-		//dd(currentNode);
-		//printTree();
-		generateAllSubsequences();
-		findDistinctNodes(currentNode);
-		/*printTree();
-		cout << "Different Nodes: " << endl;
-		for (set<int>::iterator it = ans.begin(); it != ans.end(); ++it) {
-			cout << (*it) << " ";
-		}
-		cout << endl;*/
-		cout << ans.size() << '\n';
-	} else if (dontHaveMovUp()) {
-		cout << countDistinctSubsequences() << '\n';
-	} else {
-		findMovsLater();
-		cout << countDistinctNodes() << '\n';
-	}
+	cout << ans << '\n';
 	return 0;
 }
