11d77a73906e4a7b4c97ffb0298c7f2456d3b589
Michael Serrato <mikebsg01@gmail.com>
2024-04-13 16:51:40 -0600

    Educational Codeforces Round 164 (Rated for Div. 2) - SOLVED PROBLEMS:  A.

---
+++ CODEFORCES/Educational Codeforces Round 164 (Rated for Div. 2)/B/B.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#define optimize_io ios_base::sync_with_stdio(0);cin.tie(0);
+#define dd(x) cout << #x << ": " << x << endl;
+#define FOR(t,i,a,b) for (t i = a; i < b; ++i)
+#define RFOR(t,i,a,b) for (t i = a; i > b; --i)
+#define CFOR(t,i,a,b,c) for (t i = a; i < b; i += c)
+#define ITFOR(t,i,a,b) for (t i = a; i != b; ++i)
+#define MP make_pair
+#define X first
+#define Y second
+#define PB(s, x) s.push_back(x)
+#define VUPPER(s,x) upper_bound(ALL(s), x) - s.begin()
+#define VLOWER(s,x) lower_bound(ALL(s), x) - s.begin()
+#define INS(s, x) s.insert(x)
+#define FIND(s,x) s.find(x)
+#define ERASE(s, x) s.erase(x)
+#define UPPER(s, x) s.upper_bound(x)
+#define LOWER(s, x) s.lower_bound(x)
+#define PUSH(s, x) s.push(x)
+#define ALL(s) s.begin(), s.end()
+#define SZ size()
+#define MAXN ((int) (1e6))
+#define EPS (1e-8)
+#define INF -1
+using namespace std;
+typedef unsigned int ui;
+typedef long long int lli;
+typedef unsigned long long int ulli;
+typedef pair<lli, lli> Pair;
+typedef pair<lli, Pair> Tern;
+typedef pair<Pair, Pair> Quart;
+typedef pair<Pair, string> cPair;
+typedef vector<bool> vectorbl;
+typedef vector<int> vectori;
+typedef vector<lli> vectorlli;
+typedef vector<Pair> vectorPair;
+typedef vector<Tern> vectorTern;
+typedef set<lli> setlli;
+typedef set<Pair> setPair;
+typedef map<int, int> mapi;
+typedef map<lli, lli> maplli;
+typedef map<lli, char> cmap;
+typedef queue<int> qi;
+typedef queue<lli> qlli;
+typedef queue<Pair> qPair;
+typedef queue<cPair> cq;
+typedef priority_queue<int> pqi;
+typedef priority_queue<lli> pqlli;
+typedef priority_queue<Pair> pqPair;
+
+int T, N;
+
+int main() {
+	optimize_io
+    int x, a, lst, ans;
+	cin >> T;
+	FOR(int, t, 0, T) {
+        cin >> N;
+        lst = -1;
+        ans = N;
+
+        FOR (int, i, 0, N) {
+            cin >> x;
+
+            if (i == 0) {
+                a = x;
+            } else if (x != a) {
+                ans = min(ans, i - lst - 1);
+                lst = i;
+            }
+        }
+        ans = min(ans, N - lst - 1);
+
+        if (ans == N) {
+            cout << "-1\n";
+        } else {
+            cout << ans << "\n";
+        }
+    }
+	return 0;
+}
